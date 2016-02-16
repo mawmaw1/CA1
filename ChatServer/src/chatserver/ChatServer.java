7d9e8d8577b73bf701e022381c5cbaca7e1635db
mawmaw1 <kontakt@magnuslarsen.dk>
2016-02-16 21:16:17 +0100

    Final v1.0

--- ChatServer/src/chatserver/ChatServer.java
+++ ChatServer/src/chatserver/ChatServer.java
@@ -23,26 +23,26 @@ import shared.ProtocolStrings;
  * @author Magnus
  */
 public class ChatServer {
-
+    
     private HashMap<String, ClientHandler> map = new HashMap();
-
+    
     private static boolean keepRunning = true;
     private static ServerSocket serverSocket;
     private String ip;
     private int port;
     private int value = 1;
-
+    
     public static void stopServer() {
         keepRunning = false;
     }
-
+    
     private void runServer(String ip, int port) throws IOException {
         this.port = port;
         this.ip = ip;
-
+        
         Logger.getLogger(Log.LOG_NAME).log(Level.INFO, ("Server started. Listening on: " + port + ", bound to: " + ip));
         System.out.println("Server started. Listening on: " + port + ", bound to: " + ip);
-
+        
         try {
             serverSocket = new ServerSocket();
             serverSocket.bind(new InetSocketAddress(port));
@@ -51,19 +51,19 @@ public class ChatServer {
             System.err.println("Could not listen on port: 8087");
             System.exit(1);
         }
-
+        
         try {
             while (true) {
-
+                
                 Socket socket = serverSocket.accept();
-
+                
                 new Thread(new Runnable() {
                     public void run() {
                         try {
                             ClientHandler handler = new ClientHandler(socket, ChatServer.this);
-
+                            
                             handler.start();
-                            sendCurrentList();
+                            
                         } catch (IOException e) {
                             e.printStackTrace();
                         }
@@ -74,40 +74,53 @@ public class ChatServer {
             System.err.println("Accept failed");
             System.exit(1);
         }
-
+        
     }
-
+    
     public void removeHandler(ClientHandler h) {
-        map.remove(h);
+        map.remove(h.getClientName());
+        
+        sendCurrentList();
     }
-
+    
     public void addHandler(ClientHandler h) {
         map.put(h.getClientName(), h);
+        sendCurrentList();
     }
-
+    
     public void sendCurrentList() {
         for (ClientHandler ch : map.values()) {
             ch.send(map.keySet().toString());
         }
     }
-
-    public void send(String message) {
-
-        for (ClientHandler ch : map.values()) {
-            if (message.contains(ch.getClientName())) {
-                ch.send(message);
-            }
-        }
-        if (message.charAt(5) == '*') {
+    
+    public void send(String message, String senderName) {
+        
+        String[] parts = message.split("#");
+        
+        if (parts[1].equals("*")) {
             for (ClientHandler ch : map.values()) {
-                ch.send(message.substring(6));
+                ch.send("MESSAGE#"+senderName+"#" + parts[2]);
             }
         }
-
+        if (parts[1].length() > 1) {
+            String[] multipleMsg = parts[1].split(",");
+            
+            for (ClientHandler ch : map.values()) {
+                for (int i = 0; i < multipleMsg.length; i++) {
+                    if (multipleMsg[i].equals(ch.getClientName())) {
+                        ch.send("MESSAGE#"+senderName+"#" + parts[2]);
+                    }                    
+                }
+                
+            }
+            
+        }
+        
     }
-
+    
     public static void main(String[] args) throws IOException {
-
+        
         try {
             Log.setLogFile("logFile.txt", "ServerLog");
             String ip = args[0];
@@ -116,6 +129,6 @@ public class ChatServer {
         } finally {
             Log.closeLogger();
         }
-
+        
     }
 }
